6f1ffe9288345affc7189042090182cec827e261
Joka duarte <Joka867@hotmail.com>
2024-05-15 20:50:47 +0100

    feat: added missing files.

---
+++ java-swagger-generator/src/test/java/pt/jorgeduarte/javaswaggergenerator/JavaSwaggerGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.jorgeduarte.javaswaggergenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSwaggerGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
